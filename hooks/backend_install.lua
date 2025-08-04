4b588c1764e3364a6a8b0e9870113d6dabedd54f
Shivam Lahoti <slahoti@mathworks.com>
2025-08-04 14:27:15 +0530

    Backend plugin to download the mpm and MATLAB wih it

--- hooks/backend_install.lua
+++ hooks/backend_install.lua
@@ -2,47 +2,86 @@ function PLUGIN:BackendInstall(ctx)
     local tool = ctx.tool
     local version = ctx.version
     local install_path = ctx.install_path
-    local mpm_url = ""
 
-    -- Detect OS using path seperator
+    local cmd = require("cmd")
     local sep = package.config:sub(1,1)
     local os = ""
+    local os_type
     if sep == "\\" then
-        os = "windows"
+        os_type = "windows"
     else
-        local uname = io.open("uname"):read("*l")
-        os = uname:lower()
+        local uname = cmd.exec("uname -s"):lower()
+        if uname:find("linux") then
+            os_type = "linux"
+        elseif uname:find("darwin") or uname:find("mac") then
+            os_type = "darwin"
+        else
+            os_type = uname
+        end
+    end
+   
+
+    -- Compute parent directory
+    local parent_path = install_path:match("^(.*)[/\\][^/\\]+$")
+    local mpm_exe = os:find("windows") and (parent_path .. "\\mpm.exe") or (parent_path .. "/mpm")
+
+    local mpm_url = ""
+    -- Check if mpm already exists
+    local file = io.open(mpm_exe, "r")
+    if file then
+        file:close()
+        -- Already present, do nothing
+    else
+        
+        -- Download mpm in parent directory
+        if  os_type == "linux" then
+            mpm_url = "https://www.mathworks.com/mpm/glnxa64/mpm"
+        
+        elseif os_type == "windows" then
+            mpm_url = "https://www.mathworks.com/mpm/win64/mpm"
+        else
+            -- Default to Intel macOS
+            mpm_url = "https://www.mathworks.com/mpm/maci64/mpm"
+        end
+    
+
+        local http = require("http")
+        -- Download file
+        local err = http.download_file({
+            url = mpm_url,
+            headers = {
+                ['User-Agent'] = "mpm-download"
+            }
+        }, mpm_exe)
+
+        if err ~= nil then
+            error("Download failed: " .. err)
+        end
+    end
+
+    if os_type ~= "windows" then
+        cmd.exec('chmod +x "' .. mpm_exe .. '"')
     end
     
-    -- Set URL based on OS
-    if os:find("linux") then
-        mpm_url = "https://www.mathworks.com/mpm/glnxa64/mpm"
-    elseif os:find("darwin") or os:find("mac") then
-        -- Default to Intel macOS
-        mpm_url = "https://www.mathworks.com/mpm/maci64/mpm"
+    local file = io.open(mpm_exe, "r")
+    if file then
+        print("[DEBUG] mpm.exe successfully downloaded at: " .. mpm_exe)
+        file:close()
     else
-        mpm_url = "https://www.mathworks.com/mpm/win64/mpm"
+        print("mpm.exe was not downloaded correctly to: " .. mpm_exe)
     end
+    -- Prepare products
+    local products = "MATLAB"
+    products = products:gsub(",", " ")
 
-    return {
-        version = version
-        -- url = mpm_url
-    }
-        
-   local products = "MATLAB"
-   products = products:gsub(",", " ")
+    -- Build the install command
+    local matlab_cmd = string.format('%s install --release=%s --destination=%s --products=%s', mpm_exe, version, install_path, products)
 
-   -- Set mpm path based on OS
-   --local mpm_path = is_windows and [[C:\Users\slahoti\mpm.exe]] or "mpm"
-   local mpm_path = "mpm"
 
-   -- Build the install command
-   local cmd = string.format('%s install --release=%s --destination=%s --products=%s', mpm_path, version, install_path, products)
+    print("[DEBUG] Running: " .. matlab_cmd)
 
-   print("[DEBUG] Running: " .. cmd)
+    local result = cmd.exec(matlab_cmd)
+    
 
-   local res = os.execute(cmd)
-   if res ~= 0 then
-       error("MATLAB install failed")
-   end
+    return {}
 end
